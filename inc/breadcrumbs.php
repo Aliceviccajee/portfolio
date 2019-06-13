deb87cb0a40b6871d9a5d82c413cc7c487fbc607
Alice <alice@viccajee.com>
2019-06-13 16:32:31 +0100

    Removing un-needed files

--- inc/breadcrumbs.php
+++
@@ -1,7 +0,0 @@
-<section class="breadcrumbs">
-  <div class="container">
-    <div class="content twelve">
-      <?php if (function_exists('breadcrumbs')) breadcrumbs(); ?>
-    </div>
-  </div>
-</section>
