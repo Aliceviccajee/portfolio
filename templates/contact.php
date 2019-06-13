f29f4f75ec14c16b398235a563cfa6513a32ee36
Alice <alice@viccajee.com>
2019-06-13 17:07:39 +0100

    V 1.0.2 Contact page added

--- templates/contact.php
+++ templates/contact.php
@@ -3,8 +3,10 @@ get_header(); ?>
 
 <section class="contact">
   <div class="container">
-    <h1>Contact</h1>
-    <h2>Let's chat</h2>
+  	<div class="text">
+	    <h1>Contact</h1>
+	    <h2>If you would like to talk to me about a project please email <a href="mailto:alice@viccajee.com?Subject=Hello%20again" target="_top">alice@viccajee.com</a></h2>
+	  	</div>
   </div>
 </section>
 
