02a261412df4596916d88edb65a884c70da13f93
Alice <alice@viccajee.com>
2019-06-13 17:22:03 +0100

    v.1.0.3

--- templates/contact.php
+++ templates/contact.php
@@ -1,11 +1,13 @@
 <?php /* Template name: Contact */
-get_header(); ?>
+get_header(); 
+$email = "alice@viccajee.com";
+?>
 
 <section class="contact">
   <div class="container">
   	<div class="text">
 	    <h1>Contact</h1>
-	    <h2>If you would like to talk to me about a project please email <a href="mailto:alice@viccajee.com?Subject=Hello%20again" target="_top">alice@viccajee.com</a></h2>
+	    <h2>If you would like to talk to me about a project please email <?php if($email) { echo '<a class="email" href="mailto:'. antispambot($email,1) . '">' . $email . '</a>';} ?></h2>
 	  	</div>
   </div>
 </section>
